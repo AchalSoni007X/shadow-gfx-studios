f82760635cfb47b363b48dd6c28656b541fac902
Achal Soni <achal.official007@gmail.com>
2026-08-16 14:41:36 +0530

    Load 18px bold utility typography

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./typography-18.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
